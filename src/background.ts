192f4cf5e8209c5d8c8f1b3f5995ea1b4201749e
Jonathan Gruber <gruberjonathan@gmail.com>
2025-09-16 01:44:29 +0200

    Snail layout

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,19 @@
-type Tab = Pick<chrome.tabs.Tab, 'id' | 'title'>
+type Tab = Pick<chrome.tabs.Tab, 'id' | 'title'> & {
+  gridPosition: [number, number]
+}
 
-export type TabListMessage = { type: 'tabList'; tabs: Tab[] }
+export type TabListMessage = {
+  type: 'tabList'
+  tabs: Tab[]
+  gridDimensions: [number, number]
+}
 
 async function sendTabs() {
   const tabs = await chrome.tabs.query({})
+
+  // const gridSize = Math.ceil(Math.sqrt(tabs.length))
+  const gridDimensions: [number, number] = [10, 20]
+  const snailPositions = snailOrder(gridDimensions)
 
   const promises = []
   for (const tab of tabs) {
@@ -11,10 +21,12 @@ async function sendTabs() {
       promises.push(
         chrome.tabs.sendMessage<TabListMessage>(tab.id, {
           type: 'tabList',
-          tabs: tabs.map(t => ({
-            id: t.id,
-            title: t.title,
+          tabs: tabs.map((tab, index) => ({
+            id: tab.id,
+            title: tab.title,
+            gridPosition: snailPositions[index],
           })),
+          gridDimensions,
         }),
       )
     }
@@ -30,3 +42,31 @@ chrome.tabs.onUpdated.addListener(async () => {
 chrome.tabs.onRemoved.addListener(async () => {
   await sendTabs()
 })
+
+function snailOrder([cols, rows]: [number, number]): [number, number][] {
+  let top = 0
+  let bottom = rows - 1
+  let left = 0
+  let right = cols - 1
+
+  let result: [number, number][] = []
+  while (top <= bottom && left <= right) {
+    for (let c = left; c <= right; c++) result.push([top, c])
+    top++
+
+    for (let r = top; r <= bottom; r++) result.push([r, right])
+    right--
+
+    if (top <= bottom) {
+      for (let c = right; c >= left; c--) result.push([bottom, c])
+      bottom--
+    }
+
+    if (left <= right) {
+      for (let r = bottom; r >= top; r--) result.push([r, left])
+      left++
+    }
+  }
+
+  return result
+}
--- src/content.ts
+++ src/content.ts
@@ -10,12 +10,26 @@ chrome.runtime.onMessage.addListener((msg, _, sendResponse) => {
     let elem = document.getElementById(id)
 
     if (!elem) {
-      elem = document.createElement('ul')
+      elem = document.createElement('div')
       elem.id = id
+      elem.style.cssText = `--grid-columns: ${msg.gridDimensions[0]}; --grid-rows: ${msg.gridDimensions[1]};`
+
       document.documentElement.appendChild(elem)
     }
 
-    elem.innerHTML = msg.tabs.map(t => `<li>${t.title}</li>`).join('')
+    console.log('msg', msg.tabs)
+
+    elem.innerHTML = msg.tabs
+      .map((tab, index) => {
+        const [x, y] = tab.gridPosition
+
+        return `
+          <div style="grid-row: ${x + 1}; grid-column: ${y + 1}; background-color: lch(from cornflowerblue calc(l + ${index + 3}) c h); ">
+            <span>${tab.title}</span>
+          </div>
+         `
+      })
+      .join('')
   }
 
   sendResponse()
