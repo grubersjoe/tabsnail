69c4a1d044b44b7d8c7839e28f6482db7b89a6a5
Jonathan Gruber <gruberjonathan@gmail.com>
2025-12-15 23:51:28 +0100

    wip2

---
+++ src/components/Eye.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  type Props = {
+    style: string
+  }
+
+  let { style }: Props = $props()
+
+  let eye: HTMLElement
+  let rot = $state(0)
+</script>
+
+<svelte:window
+  on:mousemove={e => {
+    const rect = eye.getBoundingClientRect()
+    const x = rect.x + rect.width / 2
+    const y = rect.y + rect.height / 2
+    const rad = Math.atan2(e.clientX - x, e.clientY - y)
+    rot = rad * (180 / Math.PI) * -1
+  }}
+/>
+
+<div class="eye" bind:this={eye} style="{style}; transform: rotate({rot}deg);"></div>
+
+<style>
+  .eye {
+    position: relative;
+    aspect-ratio: 1;
+    width: 20px;
+    border-radius: 50%;
+    background: #eee;
+    outline: solid 1px #ddd;
+  }
+
+  /* Pupil */
+  .eye:after {
+    position: absolute;
+    aspect-ratio: 1;
+    top: 54%;
+    right: 34%;
+    width: 34%;
+    background: #000;
+    border-radius: 50%;
+    content: '';
+  }
+</style>
